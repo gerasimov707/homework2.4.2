25db9a29637fe77ae619c8ec0ee48e81a1f24bee
alexandr <gerasimov707@yandex.ru>
2022-06-05 14:20:33 +0300

    commit 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+        public static void main (String[]args){
+            boolean result = Validator.check("hgg","","u");
+
+        }
+    }
+
---
+++ src/Validator.java
@@ -0,0 +1,48 @@
+import java.util.Objects;
+
+
+public class Validator {
+    private static final String VALID_SYMBOLS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+
+    private Validator() {
+    }
+
+    public static boolean check(String login, String password, String confirmPassword) {
+        try {
+            validate(login, password, confirmPassword);
+            return true;
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    private static void validate(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
+        if (Objects.isNull(login) || login.length() > 20 || !containsValidSymbols(login)) {
+            throw new WrongLoginException("dlina logina <=20");
+        }
+        //if( checkPassword(password) || checkPassword(confirmPassword)){
+        if (isNotValidPassword(password) || isNotValidPassword(confirmPassword)) {
+            throw new WrongPasswordException("dlina logina <20");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("paroli dolzni sovpadat");
+        }
+    }
+
+    //private static boolean  checkPassword(String password) {
+    private static boolean isNotValidPassword(String password) {
+        return Objects.isNull(password) || password.length() >= 20 || !containsValidSymbols(password);
+    }
+
+    private static boolean containsValidSymbols(String s) {
+        char[] symbols = s.toCharArray();
+        for (char symbol : symbols) {
+            if (!VALID_SYMBOLS.contains(String.valueOf(symbol))) {
+                return false;
+            }
+        }
+        return false;
+    }
+}
+
